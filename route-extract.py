4e8486225175ee25ad8fec905600d7089dbc58c5
Celticcow <gregory.dunlap@gmail.com>
2020-05-18 12:17:43 -0500

    split into functions so we can do multiple

--- route-extract.py
+++ route-extract.py
@@ -19,29 +19,23 @@ gregory.dunlap / celtic_cow
 
 """
 
-def main():
-    debug = 0
-
-    ip_addr  = "146.18.96.16"
-    ip_cma   = "146.18.96.25"
-    user     = "gdunlap"
-    password = "1qazxsw2"
-
-    sid = apifunctions.login(user,password, ip_addr, ip_cma)
-
-    if(debug == 1):
-        print("session id : " + sid)
+"""
+takes fwname and returns route base64 blob
+"""
+def get_routes(fwname, ip_addr, sid):
+    debug = 1
 
     get_route_json = {
         "script-name" : "script to get route table from fw",
         "script" : "clish -c 'show configuration static-route'",
-        "targets" : ["hublab1"]
+        "targets" : [fwname]
     }
 
     route_result = apifunctions.api_call(ip_addr, "run-script", get_route_json, sid)
 
-    print(json.dumps(route_result))
-
+    if(debug == 1):
+        print(json.dumps(route_result))
+    
     #get task ID
     task_id = route_result['tasks'][0]['task-id']
 
@@ -71,32 +65,67 @@ def main():
     if(debug == 1):
         print(json.dumps(task_info))
 
-    task_len = len(task_info['tasks'][0]['task-details'])
-    
     if(debug == 1):
-        print(task_len)
+        print("-----------------------------------------\n\n")
+        print(task_info['tasks'][0]['task-details'][0]['responseMessage'])
+        print("\n\n\n")
 
-    print("-----------------------------------------\n\n")
-    print(task_info['tasks'][0]['task-details'][0]['responseMessage'])
-    a_base64_message = task_info['tasks'][0]['task-details'][0]['responseMessage']
-    print("\n\n\n")
-    #for x in range(task_len):
-    #    if(task_info['tasks'][0]['task-details'][x]['cluster'] == True):
-    #        print("*^*^*^*^*^")
-    #        print(task_info['tasks'][0]['task-details'][x]['stagesInfo'])
+    return(task_info['tasks'][0]['task-details'][0]['responseMessage'])
 
-    a_base64_bytes = a_base64_message.encode('ascii')
+## end of get_routes()
+
+def convert64(b64, search_str):
+    debug = 1
+
+    nets = list()
+
+    a_base64_bytes = b64.encode('ascii')
     a_message_bytes = base64.b64decode(a_base64_bytes)
     a_message = a_message_bytes.decode('ascii')
 
     routes = a_message.split('\n')
 
     for route in routes:
-        print(route)
-        print("----")
+               
+        if(search_str in route):
+            if(debug == 1):
+                print(route)
+                print("match")
+                print("----")
+
+            parts = route.split(" ")
+            if(debug == 1):
+                print(parts[2])
+            nets.append(parts[2])
+
+    return(nets)
+
+    
+#end of convert64()
+
+def main():
+    debug = 1
+
+    ip_addr  = "146.18.96.16"
+    ip_cma   = "146.18.96.25"
+    user     = "gdunlap"
+    password = "1qazxsw2"
+
+    networks = list()
+
+    sid = apifunctions.login(user,password, ip_addr, ip_cma)
 
     if(debug == 1):
-        print(json.dumps(task_info))
+        print("session id : " + sid)
+
+    a_base64_message = get_routes("hublab1", ip_addr, sid)
+
+    networks = convert64(a_base64_message, "161.135.150.129")
+
+    print(len(networks))
+
+    for i in range(len(networks)):
+        print(str(i) + " " + networks[i])
 
     # don't need to publish
     time.sleep(20)
